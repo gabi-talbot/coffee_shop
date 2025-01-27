ac12deb0a8b0329528ee7db855d1fdd8dc44579b
Gabi <talbotgabi@gmail.com>
2025-01-27 15:01:28 +0000

    moved environment variables to .env

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -3,11 +3,17 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+
+
+AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
+ALGORITHMS = [os.getenv("ALGORITHMS")]
+API_AUDIENCE = os.getenv("API_AUDIENCE")
 
 ## AuthError Exception
 '''
